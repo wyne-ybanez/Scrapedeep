b14bf094c7bed4bb051eaad594ef917760d6e339
wyne-ybanez <wyne_01@icloud.com>
2024-03-15 15:19:01 +0000

    update: names

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Scrapeheap</title>
+    <title>Scrapedeep | Child of Scrapeheap</title>
 </head>
 
 <body>
-    <h1>Scrapeheap</h1>
+    <h1>Scrapedeep</h1>
     <form action="action.php" method="post">
         <label for="target_url">Target URL</label>
         <input type="url" name="target_url" id="target_url">
--- src/Scrapeheap.php
+++ src/Scrapeheap.php
@@ -37,11 +37,29 @@ class Scrapeheap
         }
 
         echo '<h3>All Done!</h3>';
-        echo 'You scraped: ' . $target_url;
+        echo 'You scraped: ' . $target_url . '<br><br>';
         echo '
         <form action="action.php" method="post">
             <label for="target_url">Scrape again - Target URL</label>
             <input type="url" name="target_url" id="target_url">
+            <br>
+            <br>
+            <p>
+                Choose a format for your scraped content. <br>
+                Otherwise they will be thrown back to you as HTML files.
+            </p>
+            <input type="checkbox" name="word_doc" id="word_doc" value="word_doc">
+            <label for="word_doc">Word Doc</label>
+            <br>
+            <br>
+            <input type="checkbox" name="html" id="html" value="html">
+            <label for="html">HTML</label>
+            <br>
+            <br>
+            <input type="checkbox" name="markdown" id="markdown" value="markdown">
+            <label for="markdown">Markdown</label>
+            <br>
+            <br>
             <input type="submit" value="Scrape">
         </form>';
     }
